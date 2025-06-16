48a56dfe0b7a8c08003210244325400d9b59f75b
root <root@ip-172-31-44-8.sa-east-1.compute.internal>
2025-06-16 19:02:47 +0000

    Ajuste

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,10 @@
 from app import create_app
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
