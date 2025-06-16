fb9565849ae1b980ae4ffa257024b6ab2f6af8ae
Fellipe <felpzzlc1@gmail.com>
2025-06-16 18:06:00 -0300

    Ajuste imagens

--- frontend/app.py
+++ frontend/app.py
@@ -6,5 +6,33 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 def index():
     return render_template("index.html")
 
+@app.route("/login.html")
+def login():
+    return render_template("login.html")
+
+@app.route("/home.html")
+def home():
+    return render_template("home.html")
+
+@app.route("/realizar-doacao.html")
+def realizar_doacao():
+    return render_template("realizar-doacao.html")
+
+@app.route("/realizar-doacao-produto.html")
+def realizar_doacao_produto():
+    return render_template("realizar-doacao-produto.html")
+
+@app.route("/realizar-doacao-produto-tipo.html")
+def realizar_doacao_produto_tipo():
+    return render_template("realizar-doacao-produto-tipo.html")
+
+@app.route("/solicitar-doacao.html")
+def solicitar_doacao():
+    return render_template("solicitar-doacao.html")
+
+@app.route("/visualizar-doacao.html")
+def visualizar_doacao():
+    return render_template("visualizar-doacao.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
